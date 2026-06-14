f7f2d18fedbb258aac98d0383a3740eed69a20bb
Wadia Fatima <wadiafatima166@gmail.com>
2026-06-14 14:47:49 +0000

    Practice

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # js
 JS learning from scratch
+
+link to study conversions operations
+https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
---
+++ practice/variables.js
@@ -0,0 +1,12 @@
+const accountId = 24123
+let accountEmail = "wadiafatima@yahoo.com"
+var accountPassword = "39139"
+accountCity = "Lahore"
+let accountState;
+
+accountEmail = "fatimawadia@gmail.com"
+accountPassword = "4234135"
+accountCity = "Islamabad"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
