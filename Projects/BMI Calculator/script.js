d52682ca98b103b07ca42b506e34bc6f327c8cf2
Wadia Fatima <wadiafatima166@gmail.com>
2026-06-21 12:32:06 +0000

    Projects

--- Projects/BMI Calculator/script.js
+++ Projects/BMI Calculator/script.js
@@ -1,26 +1,30 @@
-const form = documnet.querySelector('form')
+const form = document.querySelector('form');
 
-form.addEventListener( function(values) {
-    const height = parseInt(documnet.querySelector('#heigt').values)
-    const weight = parseInt(documnet.querySelector('#weigt').values)
-    const results = documnet.querySelector('#results')
+form.addEventListener('submit', function(e) {
+    e.preventDefault(); // Prevents page reload on submit
 
-    if(height == '' || height <= 0 || isNaN(height)){
-        results.innerHTML = `Please enter a valid Height`
+    const height = parseInt(document.querySelector('#height').value);
+    const weight = parseInt(document.querySelector('#weight').value);
+    const results = document.querySelector('#results');
+
+    if (isNaN(height) || height <= 0) {
+        results.innerHTML = `Please enter a valid Height`;
     }
-    else if(weight == '' || weight <= 0 || isNaN(weight)) {
-        results.innerHTML = `Please enter a valid Weight`
+    else if (isNaN(weight) || weight <= 0) {
+        results.innerHTML = `Please enter a valid Weight`;
     }
     else {
-        const BMI = (weight / ((height*height)/10000)).toFixed(2)
-        if(BMI < 18.6){
-            results.innerHTML = `<span>${BMI}, You are underweight!!</span>`
+        const BMI = (weight / ((height * height) / 10000)).toFixed(2);
+        
+        if (BMI < 18.6) {
+            results.innerHTML = `<span>${BMI}, You are underweight!!</span>`;
         }
-        else if(BMI >= 18.6 && BMI <= 24.9){
-            results.innerHTML = `<span>${BMI}, You are in normal range!!</span>`
+        else if (BMI >= 18.6 && BMI <= 24.9) {
+            results.innerHTML = `<span>${BMI}, You are in normal range!!</span>`;
         }
         else {
-            results.innerHTML = `<span>${BMI}, You are overweight!!</span>`
+            results.innerHTML = `<span>${BMI}, You are overweight!!</span>`;
         }
+        console.log(`result: ${BMI}`);
     }
-})
+});
